7ec8fb2af5b6d42c12cdbb253a88a7139d593ed5
Patrick Cuba <cubap@slu.edu>
2025-05-15 00:19:33 -0500

    fixing collections

--- public_html/js/components/deer-list.js
+++ public_html/js/components/deer-list.js
@@ -5,7 +5,7 @@ import { getLabel } from '../utils/string-utils.js'
 
 class DeerListTemplate extends HTMLElement {
     static get observedAttributes() {
-        return [config.ID]
+        return [config.ID, config.COLLECTION]
     }
 
     async connectedCallback() {
@@ -13,18 +13,49 @@ class DeerListTemplate extends HTMLElement {
     }
 
     async attributeChangedCallback(name, oldValue, newValue) {
-        if (name === config.ID && oldValue !== newValue) {
+        if (oldValue !== newValue) {
             await this.render()
         }
     }
 
     async render() {
         let obj = JSON.parse(this.getAttribute('data-obj') || '{}')
-        if (Object.keys(obj).length === 0) {
-            const id = this.getAttribute(config.ID)
-            if (id) {
-                obj = await expand(id)
+        const collection = this.getAttribute('deer-collection')
+        if (!Object.keys(obj).length && collection) {
+            const historyWildcard = { "$exists": true, "$size": 0 }
+            const queryObj = {
+                $or: [
+                    { "targetCollection": collection },
+                    { "body.targetCollection": collection }
+                ],
+                "__rerum.history.next": historyWildcard
             }
+            const pointers = await fetch(`${config.URLS.QUERY}?limit=100`, {
+                method: "POST",
+                mode: "cors",
+                headers: { 'Content-Type': 'application/json; charset=utf-8' },
+                body: JSON.stringify(queryObj)
+            }).then(res => res.json()).catch(() => [])
+            const list = await Promise.all(
+                pointers.map(({ target, "@id": id, id: fallbackId }) => {
+                    const tid = (target || id || fallbackId)?.replace(/https?:/, 'https:')
+                    return fetch(tid).then(res => res.json()).catch(() => ({ __deleted: true }))
+                })
+            ).then(results => results.filter(item => !item.__deleted))
+            if (!list.length) {
+                this.innerHTML = `<span>No items found in the collection</span>`
+                return
+            }
+            obj = {
+                name: collection,
+                itemListElement: list,
+                "@type": list[0]?.["@type"] ?? list[0]?.type ?? "ItemList"
+            }
+        }
+
+        const id = this.getAttribute(config.ID)
+        if (id) {
+            obj = await expand(id)
         }
 
         const { list: listKey = '', link = '' } = JSON.parse(this.getAttribute('data-options') ?? '{}')
@@ -35,15 +66,20 @@ class DeerListTemplate extends HTMLElement {
             return
         }
 
+        // Use deer-link attribute if present, fallback to data-options
+        let resolvedLink = this.getAttribute('deer-link')
+        if (!resolvedLink) {
+            const options = JSON.parse(this.getAttribute('data-options') ?? '{}')
+            resolvedLink = options.link || ''
+        }
         let tmpl = `<h2>${getLabel(obj)}</h2><ul>`
         itemList.forEach((val, index) => {
             const name = getLabel(val, val.type ?? val['@type'] ?? index)
-            tmpl += val["@id"] && this.link
-                ? `<li ${config.ID}="${val["@id"]}"><a href="${this.link}${val["@id"]}">${name}</a></li>`
+            tmpl += val["@id"] && resolvedLink
+                ? `<li ${config.ID}="${val["@id"]}"><a href="${resolvedLink}${val["@id"]}">${name}</a></li>`
                 : `<li ${config.ID}="${val["@id"]}">${name}</li>`
         })
         tmpl += `</ul>`
-
         this.innerHTML = tmpl
     }
 }
